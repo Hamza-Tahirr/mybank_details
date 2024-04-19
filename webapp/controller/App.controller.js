37ea5d0ed0c96388c8c9365dad7beeca2ddb4f16
Hamza Tahir <hamzatahir.tmc@outlook.com>
2024-04-19 07:37:09 +0000

    init bindings

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,21 @@ sap.ui.define([
         return Controller.extend("com.sap.mybankdetails.controller.App", {
             onInit: function () {
 
+                let oData = {
+                    accountDetails: {
+                        AccountNum: "42015427",
+                        Name: "Hamza Tahir",
+                        Ifsc: "ABC123XYZ",
+                        Addr: "123 Maple Street",
+                        City: "Frankfurt",
+                        PosC: "73290"
+                    },
+                    // Ifscode: "090078601"   property binding
+
+                };
+                let oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData(oData)
+                this.getView().setModel(oModel);
             },
             onOpenBankDetails: function () {
                 //create dialog lazily
